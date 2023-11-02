2a1ff6d431dc59666950954bcf5a87d467fa8a8a
issmaile <ismail25smello@gmail.com>
2023-11-03 00:38:08 +0100

    .

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *n = malloc(sizeof(binary_tree_t));
 
-	if (n = NULL)
+	if (n == NULL)
 		return (NULL);
 	n->n = value;
 	n->parent = parent;
