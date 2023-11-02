b8ab0ff610a5c761ddda01bae10e6aacb7318dc9
issmaile <ismail25smello@gmail.com>
2023-11-03 00:59:51 +0100

    .

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_insert_left - inserts a node as the left child
- *@parent: a ptr to the node to insert the left child in
- *@value: value to store in the new node
-
+ * @parent: a ptr to the node to insert the left child in
+ * @value: value to store in the new node
+ *
  * Return: ptr to created node, or NULL on failure or if parent is NULL.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
