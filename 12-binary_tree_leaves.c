c9feea1d8ba77db64c153a34754025813555c71d
issmaile <ismail25smello@gmail.com>
2023-11-03 01:40:47 +0100

    .

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - checks if a node is leaf
+ * @node: ptr to node to check
+ *
+ * Return: ret 1 if leaf, 0 if not
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->right == NULL && node->left == NULL)
+		return (1);
+	else
+		return (0);
+}
+
+/**
  * binary_tree_leaves - number of leaves
  * @tree: ptr to the root node
  *
