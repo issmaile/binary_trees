9927fce9ebad3a0393f34803cf179250d1da8cab
issmaile <ismail25smello@gmail.com>
2023-11-03 01:03:59 +0100

    .

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - dels an entire bin tree
+ * @tree: ptr to root node of tree to del
+ *
+ * Return: void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree)
+	{
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
+		free(tree)
+	}
+}
